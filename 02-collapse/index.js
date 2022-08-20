bb2d6e7c14af8f13ffc7f01a9f82befff4a743a1
Ivan Kotov <ivankotov12345@gmail.com>
2022-08-20 23:20:42 +0300

    init: add test tasks with solution
    docs: update information in README file

---
+++ 02-collapse/index.js
@@ -0,0 +1,112 @@
+const wrapper = document.querySelector('.collapsible');
+const button = document.querySelector('.collapsible__button');
+const content = document.querySelector('.collapsible__content');
+const buttonLabels = document.querySelectorAll('.collapsible__action');
+const lableVisible = document.querySelector('.collapsible__action--visible');
+const lableHidden = document.querySelector('.collapsible__action--hidden');
+
+let fadeFlag = 0;
+
+const stylesWrapper = () => {
+  wrapper.style.display = 'flex';
+  wrapper.style.flexDirection = 'column';
+  wrapper.style.alignItems = 'center';
+  wrapper.style.marginTop = '50px';
+};
+
+stylesWrapper();
+
+const stylesBtn = () => {
+  button.style.backgroundColor = "green";
+  button.style.border = 'none';
+  button.style.borderRadius = '8px';
+};
+
+stylesBtn();
+
+const stylesLable = () => {
+  lableVisible.style.display = 'none';
+  lableHidden.style.display = 'block';
+  button.style.width = '300px';
+  button.style.height = '50px';
+
+  buttonLabels.forEach(lable => {
+    lable.style.fontSize = '18px';
+    lable.style.textTransform = 'uppercase';
+    lable.style.color = '#FFF';
+  });
+}
+
+stylesLable();
+
+const stylesContent = () => {
+  content.style.fontSize = '24px';
+  content.style.transform = 'translateY(-1000px)';
+}
+
+stylesContent();
+
+const animationBtn = () => {
+  const keyframesBtn = [
+    { opacity: .3 },
+    { opacity: 1 },
+  ];
+    
+  const optionsBtn = {
+    duration: 300,
+    easing: 'ease-in-out',
+  };
+
+  button.animate(keyframesBtn, optionsBtn)
+}
+
+const fadeInContent = () => {
+  content.style.transform = 'translateY(0)';
+  const keyframesFadeIn = [
+    { transform: 'translateY(-1000px)' },
+    { transform: 'translateY(0)' },
+  ];
+
+  const optionsContent = {
+    duration: 300,
+    easing: 'ease-in-out',
+  };
+
+  content.animate(keyframesFadeIn, optionsContent)
+}
+
+const fadeOutContent = () => {
+  content.style.transform = 'translateY(-1000px)';
+  const keyframesFadeIn = [
+    { transform: 'translateY(0)' },
+    { transform: 'translateY(-1000px)' },
+  ];
+
+  const optionsContent = {
+    duration: 300,
+    easing: 'ease-in-out',
+  };
+
+    content.animate(keyframesFadeIn, optionsContent)
+}
+
+const contentToggle = () => {
+  if (fadeFlag === 0) {
+    fadeFlag = 1;
+    fadeInContent();
+  } else if (fadeFlag === 1) {
+    fadeFlag = 0;
+    fadeOutContent();
+  }
+};
+
+const btnToggle = () => {
+  lableVisible.style.display = (lableVisible.style.display === 'none') ? 'block' : 'none';
+  lableHidden.style.display = (lableHidden.style.display === 'block') ? 'none' : 'block';
+  button.style.backgroundColor = (button.style.backgroundColor === 'green') ? 'red' : 'green';
+
+  contentToggle();
+  animationBtn();
+};
+
+button.addEventListener('click', btnToggle)
